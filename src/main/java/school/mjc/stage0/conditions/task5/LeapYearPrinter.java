08897a03ca8dcd02b3da99eec7a6f4f1017a95f6
Azimbek <omar.azimbek@gmail.com>
2023-12-30 03:12:06 +0600

    0.4.5 Commit

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,33 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+    public static void isLeapYear(int year) {
+        int remainderBy4 = year % 4;
+        int remainderBy100 = year % 100;
+        int remainderBy400 = year % 400;
+
+        switch (remainderBy4) {
+            case 0:
+                switch (remainderBy100) {
+                    case 0:
+                        switch (remainderBy400) {
+                            case 0:
+                                System.out.println("leap");
+                                break;
+                            default:
+                                System.out.println("not leap");
+                        }
+                        break;
+                    default:
+                        System.out.println("leap");
+                }
+                break;
+            default:
+                System.out.println("not leap");
+                break;
+        }
+    }
+    public static void main(String[] args) {
+        isLeapYear(2024);
     }
 }
